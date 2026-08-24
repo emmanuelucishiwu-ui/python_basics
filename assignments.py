e2bf1908c318bef1db1bc2869beee60385b56b17
emmanuelucishiwu-ui <emmanuelucishiwu@gmail.com>
2026-08-24 16:50:08 +0100

    assignment on variabkes

---
+++ assignments.py
@@ -0,0 +1,21 @@
+# String
+name = "Emmanuel"
+
+# Integer
+age = 18
+
+# Float
+height = 1.75
+
+# Complex Number
+complex_number = 4 + 7j
+
+# Boolean
+is_student = True
+
+# Print all variables
+print("String:", name)
+print("Integer:", age)
+print("Float:", height)
+print("Complex Number:", complex_number)
+print("Boolean:", is_student)
---
+++ variables_datatypes.py
@@ -0,0 +1,16 @@
+# variables  and datatypes
+
+#  a variables is a named container used to store and manage data in a computer memory 
+
+# name = "emmanuel"
+# print(name)
+
+
+# if name == "emmanuel":
+#     print("hello emmanuel")
+
+x = 2+2 
+print(x) 
+
+if x:
+    print(f"Hello {x} ")
